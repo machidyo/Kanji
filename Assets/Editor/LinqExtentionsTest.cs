d244943b4ddbe412fd50b1b9259ce5316909844d
machidyo <yorihiko_m@hotmail.com>
2020-04-11 21:13:34 +0900

    add: chunk する linq の extentions と合わせてテストを追加

---
+++ Assets/Editor/LinqExtentionsTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Assert = UnityEngine.Assertions.Assert;
+using Kanji.Helper;
+
+public class LinqExtentionsTest
+{
+    private readonly int[] _testData10 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    private readonly int[] _testData1 = {1};
+
+    [Test]
+    public void Chunk10Test()
+    {
+        var chunks = _testData10.Chunk(3).ToArray();
+
+        Assert.AreEqual(4, chunks.Length);
+        UnitTestHelper.AssertList(new[] {1, 2, 3}, chunks[0].ToArray());
+        UnitTestHelper.AssertList(new[] {4, 5, 6}, chunks[1].ToArray());
+        UnitTestHelper.AssertList(new[] {7, 8, 9}, chunks[2].ToArray());
+        UnitTestHelper.AssertList(new[] {10}, chunks[3].ToArray());
+    }
+
+    [Test]
+    public void Chunk1Test()
+    {
+        var chunks = _testData1.Chunk(1).ToArray();
+
+        Assert.AreEqual(1, chunks.Length);
+        UnitTestHelper.AssertList(new[] {1}, chunks[0].ToArray());
+    }
+}
---
+++ Assets/Editor/UnitTestHelper.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+// ref https://qiita.com/su10/items/67a4a90c648b1ef68ab9
+using Assert = UnityEngine.Assertions.Assert; // AssertはNUnitのではなくUnityのものを使う
+
+public class UnitTestHelper
+{
+    [TestCase(new[] {1, 2, 3}, new[] {1, 2, 3})]
+    public static void AssertList<T>(IList<T> expected, IList<T> actual)
+    {
+        Assert.AreEqual(expected.Count, actual.Count);
+        for (var i = 0; i < expected.Count; i++)
+        {
+            Assert.AreEqual(expected[i], actual[i]);
+        }
+
+        //// Assertion success.
+        // var a = new[] {1, 2, 3};
+        // var b = a;
+        // Assert.AreEqual(a, b);
+
+        //// Assertion failure. Values are not equal.
+        // Assert.AreEqual(new[] { 1, 2, 3 }, new[] { 1, 2, 3 });
+    }
+
+    [TestCase(new[] {2.160247f, 4.527693f, 3.141593f}, new[] {2.160247f, 4.527693f, Mathf.PI})]
+    public static void AreApproximatelyEqualList(IList<float> expected, IList<float> actual)
+    {
+        Assert.AreEqual(expected.Count, actual.Count);
+        for (var i = 0; i < expected.Count; i++)
+        {
+            Assert.AreApproximatelyEqual(expected[i], actual[i]);
+        }
+    }
+
+    [Test]
+    public void CallTest()
+    {
+        var expected = new[] {1, 2, 3};
+        var actual = new[] {1, 2, 3};
+        UnitTestHelper.AssertList(expected, actual);
+    }
+
+    [Test]
+    public void SimplePasses()
+    {
+        Assert.AreEqual(2, 1 + 1);
+    }
+
+    [UnityTest]
+    public IEnumerator EnumeratorPasses()
+    {
+        yield return null;
+        Assert.AreEqual(2, 1 + 1);
+    }
+}
